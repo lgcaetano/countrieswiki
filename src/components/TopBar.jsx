00677cf0a148cc944157754bcb363d60704ea492
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-09-24 22:56:26 -0300

    added a globe besides the Where In The World in the bar located at the top

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Component } from "react";
 
 export default class TopBar extends Component{
+
+    globeStyles = {
+        marginRight: "10px",
+        fontSize: "30px"
+    }
+
+
+
+
     render(){
         return <div id="top-bar" className="box-shadow">
-            <h3>Where in the world?</h3>
+            <h3><i class="fas fa-globe-americas" style={this.globeStyles}></i> Where in the world?</h3>
             <button id="dark-mode-toggle" onClick={() => this.props.toggleFunction()}><i className="fas fa-moon"></i> Dark Mode</button>
         </div>
     }
